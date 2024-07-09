a9a19d08828909817f7419dfaa84c86739984c69
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-09 17:22:49 +0200

    [UPD] Added condition to train list

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Models\Train;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Carbon;
+
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        $today = Carbon::today();
+        $trains = Train::whereDate('landing_hour', '=', $today)->get();
 
         return view('welcome', compact('trains'));
     }
