d18411d667cab524326e49bed0c60f13d030ca83
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-10 16:11:22 +0200

    [UPD] Modified card text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,16 +18,16 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="card-title bg-primary rounded text-uppercase">Codice Treno:
-                                {{ $train['train_code'] }}</div>
+                                {{ $train->train_code }}</div>
                             <p class="card-text">
-                                Agenzia: {{ $train['agency'] }} <br>
-                                Stazione di partenza: {{ $train['start_station'] }} <br>
-                                Stazione di arrivo: {{ $train['end_station'] }} <br>
-                                Data partenza: {{ $train['landing_hour'] }} <br>
-                                Data arrivo: {{ $train['arrival_hour'] }} <br>
-                                Numero Cabine: {{ $train['n_cabs'] }} <br>
-                                In orario: {{ $train['in_time'] }} <br>
-                                Cancellato: {{ $train['deleted'] }} <br>
+                                Agenzia: {{ $train->agency }} <br>
+                                Stazione di partenza: {{ $train->start_station }} <br>
+                                Stazione di arrivo: {{ $train->end_station }} <br>
+                                Data partenza: {{ $train->landing_hour }} <br>
+                                Data arrivo: {{ $train->arrival_hour }} <br>
+                                Numero Cabine: {{ $train->n_cabs }} <br>
+                                In orario: {{ $train->in_time }} <br>
+                                Cancellato: {{ $train->deleted }} <br>
                             </p>
                         </div>
                     </div>
